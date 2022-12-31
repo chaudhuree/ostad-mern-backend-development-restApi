d5f8a26266c87c6696694e5f6209837efb361a1e
chaudhuree <chaudhuree00@gmail.com>
2022-12-31 14:40:35 +0600

    docs: below comment

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(limiter);
 
 
 // mongodb connection
-mongoose.connect('mongodb://localhost:27017/Todo', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+mongoose.connect('mongodb://127.0.0.1:27017/Todo')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
