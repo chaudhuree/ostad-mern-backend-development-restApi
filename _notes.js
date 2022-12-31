16cb40a72d0f98e8954171f36b6e5a1eca5131c8
chaudhuree <chaudhuree00@gmail.com>
2022-12-31 14:03:57 +0600

    docs: basic folder structure for mern stack

---
+++ _notes.js
@@ -0,0 +1,14 @@
+// project features
+
+// a. user registration
+// b. user login
+// c. user auth by jwt
+// d. user profile read
+// e. user profile update
+// f. user todo list create
+// g. user todo list read
+// h. user todo list update
+// i. user todo list delete
+// j. user todo list complete/cancel mark
+// k. user todo list complete/cancel filter
+// l. user todo list date filter
